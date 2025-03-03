414d9a43899a6bcbba4b9ee069236ede87f3e9f1
Dirtnelle <dohnelle.deguzman@marcellin.school.nz>
2025-03-04 11:03:23 +1300

    Added some comments explaining what the code does

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ age = int(input('How old are you?: ')) #This code asks for the users age and pri
 print(f'{name}, Your {age}? You are very old')
 
 if age > 25:
-    print("Wow you are very old")
+    print("Wow you are very old") #This code prints "Wow you are very old" if you are above the age of 25
 
 if age >= 13 and age <=19:
-     print("You are a teenager like me!!")
+     print("You are a teenager like me!!") #This code prints "You are a teenager like me!!" if your input a age between 13 and 19
