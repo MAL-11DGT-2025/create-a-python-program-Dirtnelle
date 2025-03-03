05f7917338c81a20df200e3ab545daeda468c09f
Dirtnelle <dohnelle.deguzman@marcellin.school.nz>
2025-03-04 10:52:24 +1300

    Added a if statement

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 print("Hello, World!")
 print("My favourite colour is purple.")
-name = input('Whats your name?: ')
+
+name = input('Whats your name?: ') #This code asks for the users name
 print(f'Greetings {name} ')
 
-age = input('How old are you?: ')
+age = int(input('How old are you?: ')) #This code asks for the users age and prints both name and age    
 print(f'{name}, Your {age}? You are very old')
+
+if age > 25:
+    print("Wow you are very old")
