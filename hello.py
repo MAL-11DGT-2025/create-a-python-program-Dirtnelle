00ee349986ac3c20b970e5aa08bce86b08155bba
Dirtnelle <dohnelle.deguzman@marcellin.school.nz>
2025-03-06 14:40:23 +1300

    minor fix

--- hello.py
+++ hello.py
@@ -11,8 +11,8 @@ if age > 25:
     print("Wow you are very old") #This code prints "Wow you are very old" if you are above the age of 25
 
 if age >= 13 and age <=19:
-     # print("You are a teenager like me!!") #This code prints "You are a teenager like me!!" if your input a age between 13 and 19
+      print("You are a teenager like me!!") #This code prints "You are a teenager like me!!" if your input a age between 13 and 19
 
- if age >= 13 or age <=19:
+if age >= 13 or age <=19:
      print("You might be a teenager like me") 
 
