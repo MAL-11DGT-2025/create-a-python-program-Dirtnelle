7b5a2018cce76280401ad42282aea445dcb8c08b
Dirtnelle <dohnelle.deguzman@marcellin.school.nz>
2025-03-06 14:43:36 +1300

    minor fix

--- hello.py
+++ hello.py
@@ -1,18 +1,29 @@
-print("Hello, World!")
-print("My favourite colour is purple.")
+#print("Hello, World!")
+#print("My favourite colour is purple.")
 
-name = input('Whats your name?: ') #This code asks for the users name
-print(f'Greetings {name} ')
+#name = input('Whats your name?: ') #This code asks for the users name
+#print(f'Greetings {name} ')
 
-age = int(input('How old are you?: ')) #This code asks for the users age and prints both name and age    
-print(f'{name}, Your {age}? You are very old')
+#age = int(input('How old are you?: ')) #This code asks for the users age and prints both name and age    
+#print(f'{name}, Your {age}? You are very old')
 
-if age > 25:
-    print("Wow you are very old") #This code prints "Wow you are very old" if you are above the age of 25
+#if age > 25:
+    #print("Wow you are very old") #This code prints "Wow you are very old" if you are above the age of 25
 
-if age >= 13 and age <=19:
-      print("You are a teenager like me!!") #This code prints "You are a teenager like me!!" if your input a age between 13 and 19
+#if age >= 13 and age <=19:
+      #print("You are a teenager like me!!") #This code prints "You are a teenager like me!!" if your input a age between 13 and 19
 
-if age >= 13 or age <=19:
-     print("You might be a teenager like me") 
+#if age >= 13 or age <=19:
+     #print("You might be a teenager like me") 
 
+umbrella = False
+sunny = True
+
+if umbrella and sunny :
+    print("You think it's going to rain.")
+elif not umbrella and sunny 
+    print("You thimk you don't need an umbrella.")
+elif umbrella and not sunny
+    print("It must be raining.")
+elif not umbrella and not sunny
+    print("You must be drenched.")
