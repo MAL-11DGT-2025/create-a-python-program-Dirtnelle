87e6ede8f7c789175367774a7e363206e618e473
Dirtnelle <dohnelle.deguzman@marcellin.school.nz>
2025-03-07 12:53:12 +1300

    Added code to print out colours in a list

---
+++ lists.py
@@ -0,0 +1,13 @@
+colours =["red", "green", "purple", "blue", "black"]
+# print(colours[0])
+# print(colours[1])
+
+# for i in colours:
+  #  print(i)
+
+   # print(colours[-3])
+
+# print(len(colours))
+for i in colours:
+    if i =="blue":
+        print("There is blue")
